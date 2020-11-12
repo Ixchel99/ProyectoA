3324e1dfdc4a12c40a3aaa97f04da751d9d5d160
Ixchel99 <71735420+Ixchel99@users.noreply.github.com>
2020-11-11 21:25:09 -0600

    Modificacion

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function (req, res) {
-    res.write("hola mundo soy isaac e ixchel isaac2 ixchel 2\n");
-    res.write("nueva liena"); //write a response to the client
+    res.write("Hello word"); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
